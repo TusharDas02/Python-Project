612a706ba7fe25fa2a4b75c18e19d8a79510822a
Start <tushardas.slg2001@gmail.com>
2026-05-01 16:25:03 +0530

    Day1 Complete

---
+++ Day1.py
@@ -0,0 +1,8 @@
+# username = input("what is your name?")
+# lengthh_of_name = len(username)
+# print(username + " has " + str(lengthh_of_name) + " characters")
+
+print("Welcome to the Band Name Generator.")
+CityName= input("Which city did you grow up in?\n")
+PetName = input("What is the name of your pet?\n")
+print("Your band name could be " + CityName + " " + PetName)

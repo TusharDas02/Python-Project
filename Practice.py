9106ae66517ba52d3dfa149dde0ca8e61de86fb5
Start <tushardas.slg2001@gmail.com>
2026-05-04 14:14:33 +0530

    complete

--- Practice.py
+++ Practice.py
@@ -1,46 +1,7 @@
-# # # # username = input("what is your name?")
-# # # # lengthh_of_name = len(username)
-# # # # print(username + " has " + str(lengthh_of_name) + " characters")
-
-# # # # print("Welcome to the Band Name Generator.")
-# # # # CityName= input("Which city did you grow up in?\n")
-# # # # PetName = input("What is the name of your pet?\n")
-# # # # print("Your band name could be " + CityName + " " + PetName)
-
-# # # print("Hello"[-2])
-
-# # len("123_123_456")
-
-# # print(type(12))
-# # print(type(12.5))
-# # print(type(True))
-# # print(type("12"))
-
-# # No_of_Letters = len(input("Enter your name:"))
-
-# # Question = "Number of letters in your name: "
-
-# # print(type(No_of_Letters))
-
-# # print(type(Question))
-
-# # # print(Question + str(No_of_Letters))
-
-# # bmi = 84/ 1.65**2
-
-# # print(round(bmi))
-
-# score = 1
-
-# score *= 10
-
-# print(score)           
-
-# print("Your score is " + str(score))
-
-
-score = 0
-height = 1.8
-is_Winning = True
-
-print(f"Your score is {score}, your hhheight is {height}, your status is {is_Winning}")
+print("Welcome to Tip Calculator!")
+bill = int(input("What was the total bill? Rupees "))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+percent_bill = bill + (bill*tip/100)
+NoOfPeople= int(input("How many people to split the bill? "))
+new_bill = round(percent_bill/NoOfPeople,2)
+print(f"Each person should pay: Rupees {new_bill}, the total bill is Rupees {percent_bill}")

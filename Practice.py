0372fef86316b66844fe6a9b0c09aa9ce3e7eaf0
Start <tushardas.slg2001@gmail.com>
2026-05-05 11:46:01 +0530

    Pass Generator

--- Practice.py
+++ Practice.py
@@ -1,20 +1,4 @@
-# import random as r
-# friends = ["Alice", "Bob", "Charlie", "David", "Emanual"]
-
-# friendNo = r.randint(0,4)
-# NoOfFriends = len(friends)
-# print(f"My friend is: {friends[NoOfFriends-1]}")
 
 
-# import random as r
-# friends = ["Alice", "Bob", "Charlie", "David", "Emanual"]
-
-# print(r.choice(friends))
-
-
-fruits = ["Strawberries", "Nectarines", "Apples", "Grapes","Peaches","Cherries","Pears","Tomatoes"]
-vegetables = [ "Spinach", "Kale","Celery","Potatoes"]
-
-dirty_dozen = [fruits, vegetables] 
-
-print(dirty_dozen[0])
+a = 10/2
+print(a)
---
+++ passwordGenerator.py
@@ -0,0 +1,34 @@
+import random as r
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Password Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))  
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+temp = []
+password = ''
+
+for letter in range(0, nr_letters):
+    temp += r.choice(letters)
+
+for number in range(0, nr_numbers):
+    temp += r.choice(numbers)
+
+for symbol in range(0, nr_symbols):
+    temp += r.choice(symbols)
+
+print(f"Non random Password {temp}")
+
+r.shuffle(temp)
+
+for Pass in temp:
+    password += Pass
+
+print(f"Your new Password is {password}")
+
+
+
